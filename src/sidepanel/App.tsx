bc02c61eb639a7591d82e2b6370d8f7c121d1860
FedorT22 <107561664+FedorT22@users.noreply.github.com>
2025-01-02 17:14:41 +0100

    chore: found feedbackIntegration causing issue

--- src/sidepanel/App.tsx
+++ src/sidepanel/App.tsx
@@ -1,10 +1,46 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { captureException } from "@sentry/react";
+// import { feedbackIntegration } from "@sentry/react";
+import { feedbackIntegration } from "@sentry/browser";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  const elFeedbackContainer = useRef(null);
+
+  const [bugReportForm, setBugReportForm] = useState<{
+    open: () => void;
+  } | null>(null);
+
+  useEffect(() => {
+    const feedback = feedbackIntegration({
+      autoInject: false,
+      colorScheme: "light",
+      showBranding: false,
+      showName: false,
+      showEmail: false,
+      enableScreenshot: false,
+      isNameRequired: false,
+      isEmailRequired: false,
+    });
+
+    feedback.attachTo(elFeedbackContainer.current!);
+
+    feedback
+      .createForm()
+      .then((newForm) => {
+        setBugReportForm(newForm);
+      })
+      .catch((error) => {
+        // eslint-disable-next-line
+        console.error(
+          "Failed to create sentry bug reporting integration widget",
+          error
+        );
+      });
+  }, []);
+
   const [count, setCount] = useState(0);
 
   function produceUncaughtSentryErrorFromSidePanel(): void {
@@ -44,6 +80,15 @@ function App() {
 
         <button onClick={produceUncaughtSentryErrorFromSidePanel}>
           Produce uncaught sentry error from sidePanel
+        </button>
+
+        <button
+          ref={elFeedbackContainer}
+          onClick={() => {
+            bugReportForm?.open();
+          }}
+        >
+          Report a bug
         </button>
       </div>
     </>
